4990a88ef3adc0e2bc56925267da4cf94784d4ee
gokulraj-23 <gokulraj200223@gmail.com>
2025-03-14 08:58:59 +0530

    Update CarNotFoundException.java

--- CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/exception/CarNotFoundException.java
+++ CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/exception/CarNotFoundException.java
@@ -2,7 +2,7 @@ package com.tribleCarLeaseManagementSystem.exception;
 //Custom exception for handling cases where a car is not found in the system
 public class CarNotFoundException  extends RuntimeException{
 
-	  public CarNotFoundException(String message) {
+	  public CarNotFoundException(String message){
 	        super(message);
 	  }
 }
