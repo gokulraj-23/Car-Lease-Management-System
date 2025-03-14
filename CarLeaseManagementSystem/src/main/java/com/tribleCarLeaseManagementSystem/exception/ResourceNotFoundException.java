58b577ff28c5e0bd038c8b43a8946b5466207cb7
gokulraj-23 <gokulraj200223@gmail.com>
2025-03-14 08:59:28 +0530

    Update ResourceNotFoundException.java

--- CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/exception/ResourceNotFoundException.java
+++ CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.tribleCarLeaseManagementSystem.exception;
 //Custom exception class for handling resource not found scenarios
 public class ResourceNotFoundException  extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+    public ResourceNotFoundException(String message){
         super(message);
     }
 
