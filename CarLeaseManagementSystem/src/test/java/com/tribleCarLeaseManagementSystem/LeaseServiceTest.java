1eccb8b1529876eaf4325519164f85b1cb2d73ba
gokulraj-23 <gokulraj200223@gmail.com>
2025-03-14 09:09:07 +0530

    Update LeaseServiceTest.java

--- CarLeaseManagementSystem/src/test/java/com/tribleCarLeaseManagementSystem/LeaseServiceTest.java
+++ CarLeaseManagementSystem/src/test/java/com/tribleCarLeaseManagementSystem/LeaseServiceTest.java
@@ -1,19 +1,15 @@
 package com.tribleCarLeaseManagementSystem;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import com.tribleCarLeaseManagementSystem.dto.Lease;
 import com.tribleCarLeaseManagementSystem.exception.ResourceNotFoundException;
 import com.tribleCarLeaseManagementSystem.repo.LeaseRepository;
