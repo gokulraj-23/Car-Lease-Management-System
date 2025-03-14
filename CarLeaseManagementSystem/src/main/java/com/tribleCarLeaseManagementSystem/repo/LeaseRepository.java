600987c8a12caf9a37b67e7eef82030fe6b86e99
gokulraj-23 <gokulraj200223@gmail.com>
2025-03-14 09:00:56 +0530

    Update LeaseRepository.java

--- CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/repo/LeaseRepository.java
+++ CarLeaseManagementSystem/src/main/java/com/tribleCarLeaseManagementSystem/repo/LeaseRepository.java
@@ -1,7 +1,5 @@
 package com.tribleCarLeaseManagementSystem.repo;
-
 import java.rmi.dgc.Lease;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LeaseRepository extends JpaRepository<Lease, Long>{
